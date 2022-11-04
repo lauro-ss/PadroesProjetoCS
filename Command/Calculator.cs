da1ce30a40d820894e32c5f61411a3ecff0e5092
Lauro Santana Silva <bestlaurobr@gmail.com>
2022-11-04 08:12:34 -0300

    Padrao Command

--- Command/Calculator.cs
+++ Command/Calculator.cs
@@ -31,7 +31,11 @@ namespace Command.RealWorld
 
             user.Compute('*', 7);
 
-            
+            user.Compute('+', 4);
+
+            user.Compute('+', 11);
+
+
 
             // Undo 4 commands
 
